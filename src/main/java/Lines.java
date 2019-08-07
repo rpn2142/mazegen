3e097f92a64361ddecb231c4dfa856a92c24094a
purushothaman.ramraj <purushothaman.ramraj@glassdoor.com>
2019-08-07 12:18:50 -0700

    add images and thicken line and save maze to jpg file

--- src/main/java/Lines.java
+++ src/main/java/Lines.java
@@ -1,10 +1,20 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Lines extends JComponent{
+
+    public static final int MARGIN = 50;
+    public static final int N = 3;
+    public static final int GAP = 150;
 
     private static class Line{
         final int x1;
@@ -44,60 +54,85 @@ public class Lines extends JComponent{
         super.paintComponent(g);
         for (Line line : lines) {
             g.setColor(line.color);
-            g.drawLine(line.x1, line.y1, line.x2, line.y2);
+            //g.drawLine(line.x1, line.y1, line.x2, line.y2);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+            g2.draw(new Line2D.Float(line.x1, line.y1, line.x2, line.y2));
         }
+
+        addImages(g, this);
+
     }
 
     public static void main(String[] args) {
-        JFrame testFrame = new JFrame();
-        testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         final Lines comp = new Lines();
         comp.setPreferredSize(new Dimension(1800, 1800));
+        initComponent(comp);
+
+
+
+        JFrame testFrame = new JFrame();
+        testFrame.setBackground(Color.LIGHT_GRAY);
+        //testFrame.setUndecorated(true);
         testFrame.getContentPane().add(comp, BorderLayout.CENTER);
-        JPanel buttonsPanel = new JPanel();
-        JButton newLineButton = new JButton("New Line");
-        JButton clearButton = new JButton("Clear");
-        buttonsPanel.add(newLineButton);
-        buttonsPanel.add(clearButton);
-        testFrame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
-        newLineButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                int[][] maze = mazeGenerator.printMaze(3);
-                for( int i=0; i<maze.length; i++)
-                    for( int j=0; j<maze.length; j++) {
-                        int square = maze[i][j];
-                        if( (square&0b0001) == 1 )
-                            drawLine(comp, i, j,i+1 , j);
-                        if( (square&0b0010) > 0 )
-                            drawLine(comp, i+1, j,i+1 , j+1);
-                        if( (square&0b0100) > 0 )
-                            drawLine(comp, i, j+1,i+1 , j+1);
-                        if( (square&0b1000) > 0 )
-                            drawLine(comp, i, j,i , j+1);
-                    }
-            }
-        });
-        clearButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                comp.clearLines();
-            }
-        });
         testFrame.pack();
-        testFrame.setVisible(true);
+        BufferedImage bi = new BufferedImage(comp.getWidth(), comp.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = bi.createGraphics();
+        comp.print(graphics);
+        graphics.dispose();
+        testFrame.dispose();
+
+        File outputfile = new File("saved.png");
+        try {
+            ImageIO.write(bi, "png", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initComponent(Lines comp) {
+        int[][] maze = mazeGenerator.printMaze(N);
+        for( int i=0; i<maze.length; i++)
+            for( int j=0; j<maze.length; j++) {
+                int square = maze[i][j];
+                if( (square&0b0001) == 1 )
+                    drawLine(comp, i, j,i+1 , j);
+                if( (square&0b0010) > 0 )
+                    drawLine(comp, i+1, j,i+1 , j+1);
+                if( (square&0b0100) > 0 )
+                    drawLine(comp, i, j+1,i+1 , j+1);
+                if( (square&0b1000) > 0 )
+                    drawLine(comp, i, j,i , j+1);
+            }
+
+    }
+
+    private static void addImages(Graphics comp, ImageObserver io) {
+
+        comp.drawImage(getImage("bee.jpg"), MARGIN+20, MARGIN+20, io);
+        comp.drawImage(getImage("flower.png"), MARGIN+(N-1)*GAP +20, MARGIN+(N-1)*GAP +15, io);
+
+    }
+
+    private static BufferedImage getImage(String name) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new FileInputStream(name));
+        }catch(FileNotFoundException e) {
+            System.out.println("Could not find file!");
+            e.printStackTrace();
+        }catch(IOException e) {
+            System.out.println("Could not read file!");
+            e.printStackTrace();
+        }
+        return image;
     }
 
     private static void drawLine(Lines comp, int srcX, int srcY, int destX, int destY) {
-        int margin = 50;
-        int gap = 100;
-        int x1 = margin+srcY*gap;
-        int x2 = margin+destY*gap;
-        int y1 = margin+srcX*gap;
-        int y2 = margin+destX*gap;
+        int x1 = MARGIN +srcY* GAP;
+        int x2 = MARGIN +destY* GAP;
+        int y1 = MARGIN +srcX* GAP;
+        int y2 = MARGIN +destX* GAP;
         comp.addLine(x1, y1, x2, y2, Color.BLACK);
     }
 
